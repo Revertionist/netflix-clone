5ed108f41923b1ff9bec74a0e797062c526827ad
TRev <govindrnair.rev@gmail.com>
2022-12-04 16:44:25 +0530

    added youtube links

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './RowPost.css'
+import YouTube from 'react-youtube';
 import { useEffect, useState } from 'react'
 import axios from '../../axios'
 import { imageUrl } from '../../Constants/constants'
@@ -12,16 +13,25 @@ function RowPost(props) {
       setMovies(response.data.results)
     })
   }, [props.url])
+
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 0,
+    },
+  };
+
   return (
     <div className='row'>
       <h2>{props.title}</h2>
       <div className='posters'>
         {movies.slice(0, 10).map((obj) =>
+
           <img className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl + obj.backdrop_path}`} alt='poster' />
         )}
-
-
-
+        <YouTube opts={opts} videoId="I9B6rwW35GQ" />
       </div>
     </div>
   )
