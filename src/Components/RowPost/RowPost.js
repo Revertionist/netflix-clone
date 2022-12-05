4874079d520f72b67e98dace76a2db02a9d0bc74
TRev <govindrnair.rev@gmail.com>
2022-12-05 23:24:45 +0530

    Changed youtube video properties

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -3,10 +3,11 @@ import './RowPost.css'
 import YouTube from 'react-youtube';
 import { useEffect, useState } from 'react'
 import axios from '../../axios'
-import { imageUrl } from '../../Constants/constants'
+import { imageUrl, API_KEY } from '../../Constants/constants'
 
 function RowPost(props) {
   const [movies, setMovies] = useState([])
+  const [urlId, setId] = useState('')
   useEffect(() => {
     axios.get(props.url).then(response => {
       console.log(response.data)
@@ -16,23 +17,34 @@ function RowPost(props) {
 
   const opts = {
     height: '390',
-    width: '640',
+    width: '100%',
     playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 0,
+      autoplay: 1,
     },
   };
-
+  const handleMovie = (id) => {
+    console.log(id)
+    axios.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response => {
+      if (response.data.results.length !== 0) {
+        setId(response.data.results[0])
+      } else {
+        alert ('Trailer not available')
+      }
+    })
+  }
   return (
     <div className='row'>
       <h2>{props.title}</h2>
       <div className='posters'>
         {movies.slice(0, 10).map((obj) =>
 
-          <img className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl + obj.backdrop_path}`} alt='poster' />
+          <img onClick={() => handleMovie(obj.id)} className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl + obj.backdrop_path}`} alt='poster' />
         )}
-        <YouTube opts={opts} videoId="I9B6rwW35GQ" />
       </div>
+      {
+        urlId && <YouTube opts={opts} videoId={urlId.key} />
+      }
+
     </div>
   )
 }
